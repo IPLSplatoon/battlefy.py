6c5b295a41a6ae7ebfc8e3e24d8eee42ae6a01f0
Vincent Lee <vlee@vlee.me.uk>
2024-01-18 22:37:48 +0000

    Add getting Org info and tournament listing data

--- battlefy/models/__init__.py
+++ battlefy/models/__init__.py
@@ -3,3 +3,4 @@ from .tournament.stage import Stage
 from .tournament.team import Team, Player
 from .tournament.match import Match
 from .tournament.standing import Standing, LadderStanding, SwissStanding, BracketStanding, RoundRobinStanding
+from .organization import Organization, organization_from_dict, SearchTournament, tournaments_from_org_search
